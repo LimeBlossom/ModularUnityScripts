ccd549dcff044b4b359dd03583c0c91cc58eaaf9
unknown <limeblossomgaming@gmail.com>
2022-09-11 14:22:30 +0100

    Added OnStart script.

---
+++ ModularTriggers/OnStart.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OnStart : MonoBehaviour
+{
+    [SerializeField] private UnityEvent events;
+    [SerializeField] private MonoBehaviour[] actions;
+
+    private void Start()
+    {
+        events.Invoke();
+        if (actions.Length > 0)
+        {
+            foreach (IActivatable action in actions)
+            {
+                action.Activate();
+            }
+        }
+    }
+}
