561ea21c401720471bb4b682a7ff2e3c0ea3c075
unknown <limeblossomgaming@gmail.com>
2022-09-15 04:49:10 +0100

    Added an interface for being moved by another object via code. First use-case is slippery ice floor.

---
+++ ModularInterfaces/IForcedMovement.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IForcedMovement
+{
+    void ForcedMoveTo(Vector3 destination);
+    void ForcedStopMoving();
+}
