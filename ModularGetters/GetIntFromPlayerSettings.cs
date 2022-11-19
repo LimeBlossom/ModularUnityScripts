2f4098403b6c4dfd48cf40936eece5968cae3949
unknown <limeblossomgaming@gmail.com>
2022-11-19 04:16:44 +0000

    Added new utilities.

---
+++ ModularGetters/GetIntFromPlayerSettings.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetIntFromPlayerSettings : MonoBehaviour, IActivatable
+{
+    [SerializeField] private string intName;
+    [SerializeField] private MonoBehaviour[] intSettables;
+
+    [SerializeField] private bool debug = false;
+
+    public void Activate()
+    {
+        int toPass = PlayerPrefs.GetInt(intName);
+        foreach (MonoBehaviour behaviour in intSettables)
+        {
+            var settable = behaviour as ISettableInt;
+            if (settable != null)
+            {
+                settable.SetInt(toPass);
+            }
+        }
+    }
+}
---
+++ ModularTriggers/OnButton.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OnButton : MonoBehaviour
+{
+    [SerializeField] private string[] buttons;
+    [SerializeField] private UnityEvent events;
+    [SerializeField] private MonoBehaviour[] actions;
+
+    [SerializeField] private bool onDown = false;
+    [SerializeField] private bool onUp = false;
+
+    void Update()
+    {
+        bool canActivate = false;
+        foreach (string button in buttons)
+        {
+            if (!onDown && !onUp && Input.GetButton(button))
+            {
+                canActivate = true;
+                break;
+            }
+            else if (onDown && Input.GetButtonDown(button))
+            {
+                canActivate = true;
+                break;
+            }
+            else if (onUp && Input.GetButtonUp(button))
+            {
+                canActivate = true;
+                break;
+            }
+        }
+
+        if (canActivate)
+        {
+            Activate();
+        }
+    }
+
+    public void Activate()
+    {
+        events.Invoke();
+        if (actions != null && actions.Length > 0)
+        {
+            foreach (IActivatable action in actions)
+            {
+                action.Activate();
+            }
+        }
+    }
+}
