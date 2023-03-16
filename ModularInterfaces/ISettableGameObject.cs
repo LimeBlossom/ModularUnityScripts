3c41393df5dec9be098284dfa44a804baad2cf3d
unknown <limeblossomgaming@gmail.com>
2023-03-16 04:20:13 +0100

    Moved settable interfaces into a single class.

--- ModularInterfaces/ISettableGameObject.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface ISettableGameObject
-{
-    void SetGameObject(GameObject setTo);
-}
---
+++ ModularInterfaces/Settables.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ISettableInt
+{
+    void SetInt(int setTo);
+}
+
+public interface ISettableFloat
+{
+    void SetFloat(float setTo);
+}
+
+public interface ISettableGameObject
+{
+    void SetGameObject(GameObject setTo);
+}
+
+public interface ISettableColor
+{
+    void SetColor(Color setTo);
+}
