a1992cc46f3161db8b54015938574a46d1fc4501
unknown <limeblossomgaming@gmail.com>
2023-03-16 04:22:17 +0100

    New script to set material color.

---
+++ ModularActions/SetMaterialProperties.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetMaterialProperties : MonoBehaviour, IActivatable, ISettableColor
+{
+    [SerializeField] private Material material;
+    [SerializeField] private Color color;
+
+    public void Activate()
+    {
+        material.SetColor("_Color", color);
+    }
+
+    public void SetColor(Color setTo)
+    {
+        color = setTo;
+    }
+}
