57911a4cbd6b19add2bec1175cc36916331959bf
unknown <limeblossomgaming@gmail.com>
2023-08-16 16:52:23 +0100

    Added User.cs that TwitchUser.cs references.

---
+++ Twitch/User.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class User
+{
+    public string id = "";
+    public string username = "";
+    public bool isBroadcaster = false;
+    public bool isMod = false;
+    public bool isSub = false;
+    public bool isGifter = false;
+}
