7ef7dc1d67b1c90f3ffd1c1495f3d83a1ad79f44
LimeBlossom <limeblossomstudio@gmail.com>
2024-02-14 00:52:54 +0000

    Added meta files

--- Tweens/TweenScaleUI.cs
+++ Tweens/TweenScaleUI.cs
@@ -1,31 +1,31 @@
-﻿using DG.Tweening;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Events;
-using UnityEngine.UI;
+﻿//using DG.Tweening;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.Events;
+//using UnityEngine.UI;
 
 
-public class TweenScaleUI : MonoBehaviour, IActivatable
-{
-    [SerializeField] private RectTransform toTween;
-    [SerializeField] private float endScale = 1;
-    [SerializeField] private float duration = 1;
-    [SerializeField] private UnityEvent onTweenEnd;
+//public class TweenScaleUI : MonoBehaviour, IActivatable
+//{
+//    [SerializeField] private RectTransform toTween;
+//    [SerializeField] private float endScale = 1;
+//    [SerializeField] private float duration = 1;
+//    [SerializeField] private UnityEvent onTweenEnd;
 
-    public void Activate()
-    {
-        if(toTween == null)
-        {
-            toTween = GetComponent<RectTransform>();
-        }
-        toTween.DOScale(endScale, duration);
-        StartCoroutine(OnTweenEnd());
-    }
+//    public void Activate()
+//    {
+//        if(toTween == null)
+//        {
+//            toTween = GetComponent<RectTransform>();
+//        }
+//        toTween.DOScale(endScale, duration);
+//        StartCoroutine(OnTweenEnd());
+//    }
 
-    IEnumerator OnTweenEnd()
-    {
-        yield return new WaitForSecondsRealtime(duration);
-        onTweenEnd.Invoke();
-    }
-}
+//    IEnumerator OnTweenEnd()
+//    {
+//        yield return new WaitForSecondsRealtime(duration);
+//        onTweenEnd.Invoke();
+//    }
+//}
